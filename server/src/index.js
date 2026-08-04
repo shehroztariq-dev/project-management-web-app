b1173305c62950e36c31880cbd13e3cb0a5bc667
Shehroz Tariq <shehroztariq.dev@gmail.com>
2026-08-04 21:29:27 +0500

    inngest integration for background jobs

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import "dotenv/config";
 import cors from "cors";
 import { clerkMiddleware } from "@clerk/express";
+import { serve } from "inngest/express";
+import { inngest, functions } from "../inngest/index";
 
 const app = express();
 
@@ -12,6 +14,7 @@ app.use(clerkMiddleware());
 app.get("/", (req, res) => {
   res.send(`Server is live!`);
 });
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 const PORT = process.env.PORT || 5000;
 
