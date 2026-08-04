10b8939add26428035fd65baf3082afb1f18ae76
Shehroz Tariq <shehroztariq.dev@gmail.com>
2026-08-04 19:07:14 +0500

    clerk auth setup

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,17 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { store } from './app/store.js'
-import { Provider } from 'react-redux'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "./app/store.js";
+import { Provider } from "react-redux";
+import { ClerkProvider } from "@clerk/react";
 
-createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-            <Provider store={store}>
-                <App />
-            </Provider>
-    </BrowserRouter>,
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <ClerkProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ClerkProvider>
+  </BrowserRouter>,
+);
--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
+
 const app = express();
 
-const PORT = 3000;
+app.use(express.json);
+app.use(cors());
+app.use(clerkMiddleware());
 
 app.get("/", (req, res) => {
-  res.send(`Project Management`);
+  res.send(`Server is live!`);
 });
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`server running on PORT:${PORT}`);
